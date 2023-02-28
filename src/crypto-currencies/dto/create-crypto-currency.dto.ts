e786a37c6851f0299d0e330c8d5f2790d7d08d06
kiarashatri <kiarashatri@gmail.com>
2023-03-01 00:58:11 +0330

    feature: validator add for CREATE and UPDATE

--- src/crypto-currencies/dto/create-crypto-currency.dto.ts
+++ src/crypto-currencies/dto/create-crypto-currency.dto.ts
@@ -1,12 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, Length } from 'class-validator';
 
 export class CreateCryptoCurrencyDto {
-  @ApiProperty()
+  @ApiProperty({ example: 'kiarash', maxLength: 10, minLength: 3 })
+  @Length(3, 10)
   name: string;
 
-  @ApiProperty()
+  @ApiProperty({ example: 'kiarashatri@gmail.com' })
+  @IsEmail()
   email: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({
+    required: false,
+    example: 'sample description',
+    maxLength: 512,
+    minLength: 3,
+  })
+  @Length(3, 512)
   description?: string;
 }
